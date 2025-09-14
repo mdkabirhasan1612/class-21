9d34826880073ddce6211db0f147a6c6d8f9fc8b
MD Hasan Al Hosainul Kabir <mdkabirhasan1612@gmail.com>
2025-09-15 01:11:44 +0600

    first commit

--- js/script.js
+++ js/script.js
@@ -1,54 +1,11 @@
-$(function(){
-    $("#hide").click(function(){
-        $(".box").hide(1000)
-    })
-})
-$(function(){
-    $("#show").click(function(){
-        $(".box").show(1000)
-    })
-})
-$(function(){
-    $("#hideShow").click(function(){
-        $(".box").toggle(1000)
-    })
-})
-$(function(){
-    $("#fadeOut").click(function(){
-        $(".box").fadeOut(1000)
-    })
-})
-$(function(){
-    $("#fadeIn").click(function(){
-        $(".box").fadeIn(1000)
-    })
-})
-$(function(){
-    $("#slideUpDown").click(function(){
-        $(".box").slideToggle(1000)
-    })
-})
-$(function() {
-    const countDownDate = new Date("Dec 25, 2025 00:00:00").getTime();
-    const x = setInterval(function() {
-        const now = new Date().getTime();
-        const distance = countDownDate - now;
-        const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-        $("#days").html(days);
-        $("#hours").html(hours);
-        $("#minutes").html(minutes);
-        $("#seconds").html(seconds);
-        if (distance < 0) {
-            clearInterval(x);
-            $("#countdownpart").html("<h2>COUNTDOWN FINISHED!</h2>");
-            $("#countdownpart h2").css({
-                "color": "#fff",
-                "text-align": "center",
-                "font-size": "2em"
-            });
-        }
-    }, 1000);
+new VenoBox({
+  selector: ".my-image-links",
+  numeration: true,
+  infinigall: true,
+  share: true,
+  spinner: "rotating-plane",
+});
+
+new VenoBox({
+  selector: ".my-video-links",
 });
